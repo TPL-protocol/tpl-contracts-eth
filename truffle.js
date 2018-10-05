514200451a5678b078524e39258c9233965f4758
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-10-05 18:45:42 +0200

    Rename development to local network

--- truffle.js
+++ truffle.js
@@ -1,13 +1,12 @@
-var Web3 = require('web3')
-
 module.exports = {
   networks: {
-    development: {
-      provider: new Web3('ws://localhost:8545'),
-      network_id: "*", // Match any network id
+    local: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
       gasPrice: 10 ** 9,
       gas: 6000000
-    }
+    },
   },
   solc: {
     optimizer: {
@@ -15,4 +14,4 @@ module.exports = {
       runs: 500
     }
   }
-};
+}
