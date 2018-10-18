5ec04a7d10a88ab382c8bbd5e01cec4aa0af1743
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-10-18 17:09:01 +0200

    Add deployment npm scripts

--- truffle.js
+++ truffle.js
@@ -7,6 +7,41 @@ module.exports = {
       network_id: "*", // Match any network id
       gasPrice: 10 ** 9,
       gas: 6000000
+    },
+    local: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      gas: 6000000,
+      gasPrice: 2e9, // 2 Gwei
+    },
+    ropsten: {
+      host: 'localhost',
+      port: 8565,
+      network_id: 3,
+      gas: 500000,
+      gasPrice: 10e9, // 10 Gwei
+    },
+    rinkeby: {
+      host: 'localhost',
+      port: 8565,
+      network_id: 4,
+      gas: 500000,
+      gasPrice: 10e9, // 10 Gwei
+    },
+    kovan: {
+      host: 'localhost',
+      port: 8555,
+      network_id: 42,
+      gas: 2000000,
+      gasPrice: 10e9, // 10 Gwei
+    },
+    mainnet: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '1',
+      gas: 1000000,
+      gasPrice: 1e9,  // 1 Gwei
     }
   },
   solc: {
